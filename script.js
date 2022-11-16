167377b2093400cafb5386080c92f3cdb8da7ed8
Corin Baurmann <corin.baurmann@gmail.com>
2022-11-16 17:17:55 +0100

    start connecting backend and frontend

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
 document.addEventListener('alpine:init', async () => {
-    if (Alpine.router.is('/:uri([a-zA-Z0-9_-]{6})')) { //TODO
+    
+    Alpine.store('links', [])
+    Alpine.store('selected', new Map())
+    Alpine.store('bunch', new Bunch())
+})
 
-        Alpine.store('links', [])
-        Alpine.store('selected', new Map())
+document.getElementById('bunch-header').addEventListener('build', (e) => {
+    router = e.detail.router;
+    
+    if (router.is('/:uri([a-zA-Z0-9_-]{6})')) {
+        path = router.path.slice(1)
+        fetchBunch(router.path.slice(1))
+    }
+}); 
 
-        Alpine.store('links', [{ id: 1, url: 'http://google.com', title: 'Google' }, { id: 2, url: 'http://bing.com', title: 'Bing' }, { id: 3, url: 'http://yahoo.com', title: 'Yahoo' }])
+let path = ''
 
+class Bunch {
+    title;
+    description;
+    username;
+    open_graph;
+
+    constructor(){
+        this.title = ''
+        this.description = null
+        this.username = null
+        this.open_graph = false,
+        this.date = ''
     }
 
+    set date (a){
+        let d = new Date(a[0], 0, a[1])
+        this.dateVal = d;
+    }
+    get date (){
+        return this.dateVal.toLocaleDateString('de-DE', {dateStyle: "medium"})
+    }
+}
+
+class Link {
+    description;
+    url;
+
+
+    get title (){
+        return this.titleVal ? this.titleVal : this.url
+    }
+    set title (t){
+        this.titleVal = t;
+    }
+}
+
+const fetchBunch = async uri => {
+    let response = await fetch(`/api/${uri}`)
+    let bunch = await response.json()
+
+    window.bunch = bunch
+    bunch = Object.assign(new Bunch, bunch);
     
-})
+    Alpine.store('links', bunch.entries.map(e => Object.assign(new Link, e)))
+    Alpine.store('bunch', bunch)
+}
+
 
 
 const selectAll = e => {
@@ -39,4 +92,9 @@ const openLinks = () => {
         window.open(link.url, '_blank');
     }
 
+}
+
+const sendClicked = (entry) => {
+    fetch(`/api/${path}/clicked/${entry.id}`, {method: 'POST'})
+    window.open(entry.url, '_blank')
 }
